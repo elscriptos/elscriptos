f0ae961f2d82cf0f4efa8c680d1aa0a947b989c4
elscriptos <elscriptos@protonmail.com>
2020-07-27 12:34:07 +0200

    version 0.0.4 fix comments :{code}: stickers replacements

--- src/services/input.js
+++ src/services/input.js
@@ -1,19 +1,34 @@
 import { editor, preview } from './elements'
 import { getContentStickers, getPreview, makeGetSavedSticker } from './stickers'
 
-function replaceContentWithStickers(inputValue, contentStickers, separator) {
-  return contentStickers.reduce((acc, contentSticker) =>
-    acc.replace(
-      new RegExp(`${separator}${contentSticker.code}${separator}`, 'gm'),
-      contentSticker.url
-    ),
-    inputValue
-  )
+function replaceContentWithStickers(inputLines, contentStickers, separator) {
+  const replaceCodeWithStickers = line =>
+    contentStickers.reduce((acc, contentSticker) =>
+      acc.replace(
+        new RegExp(`${separator}${contentSticker.code}${separator}`, 'gm'),
+        contentSticker.url
+      ),
+      line
+    )
+
+  return inputLines
+    .map(line =>
+      isCommentLine(line)
+        ? line
+        : replaceCodeWithStickers(line)
+    )
+    .join('\n')
 }
 
-function getStickerMatches(inputValue, separator) {
+const getInputLines = inputValue => inputValue.split('\n')
+
+const isCommentLine = line => typeof line === 'string' && line[0] === '>'
+
+const filterCommentLines = inputLines => inputLines.filter(line => !isCommentLine(line))
+
+function getStickerMatches(value, separator) {
   const matchCleanRegExp = new RegExp(`${separator}(.*)${separator}`, 'gm')
-  return (inputValue.match(new RegExp(`${separator}[^\n${separator} ]+${separator}`, 'gm')) || [])
+  return (value.match(new RegExp(`${separator}[^\n${separator} ]+${separator}`, 'gm')) || [])
     .map(match => match.replace(matchCleanRegExp, '$1'))
 }
 
@@ -23,7 +38,11 @@ export function fillInput({
   userStickers,
   separator
 }) {
-  const matchs = getStickerMatches(inputValue, separator)
+  const inputLines = getInputLines(inputValue)
+  const matchs = getStickerMatches(
+    filterCommentLines(inputLines).join('\n'),
+    separator
+  )
   if (matchs.length === 0) return;
   const getSavedSticker = makeGetSavedSticker(cache, userStickers, separator)
   const contentStickers = matchs
@@ -33,7 +52,7 @@ export function fillInput({
       code: match
     }))
     .filter(data => data.url != null)
-  editor.value = replaceContentWithStickers(inputValue, contentStickers, separator)
+  editor.value = replaceContentWithStickers(inputLines, contentStickers, separator)
 }
 
 export async function fillPreview({
@@ -42,9 +61,13 @@ export async function fillPreview({
   userStickers,
   separator
 }) {
-  const matchs = getStickerMatches(inputValue, separator)
+  const inputLines = getInputLines(inputValue)
+  const matchs = getStickerMatches(
+    filterCommentLines(inputLines).join('\n'),
+    separator
+  )
   const contentStickers = await getContentStickers(matchs, cache, userStickers, separator)
-  const content = replaceContentWithStickers(inputValue, contentStickers, separator)
+  const content = replaceContentWithStickers(inputLines, contentStickers, separator)
   const previewHTML = await getPreview(content)
   preview.innerHTML = previewHTML
 
